a8ce6399b14c8a46c651874bb7663e387c0981c6
Ahmad2Anzar <161143818+Ahmad2Anzar@users.noreply.github.com>
2024-10-29 00:12:57 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ workers/email_worker.js
@@ -0,0 +1,13 @@
+
+const comment_mailer = require('../mailers/comment_mailer');  
+
+queue.process('emails', function(job, done) {
+    try {
+        console.log('emails worker is doing the job', job.data);
+        comment_mailer.newComment(job.data);
+        done();
+    } catch (error) {
+        console.error('Error processing job:', error);
+        done(error); // Pass the error to mark the job as failed
+    }
+});
